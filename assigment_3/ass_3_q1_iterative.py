943e30583b92a8e48016b8904f8b10d39d2c9710
Espen <espen.l@hotmail.com>
2024-09-12 09:31:48 +0200

    separated q1 into two files on assignment 3

---
+++ assigment_3/ass_3_q1_iterative.py
@@ -0,0 +1,19 @@
+#Iterative approach
+def is_Palindrome(word):
+    front = 0
+    back = len(word) - 1
+
+    while front != back:
+        
+        if word[front] != word[back]:
+            return print("False")
+        
+        else:
+            front += 1
+            back -=1
+    print("True")
+        
+        
+    
+is_Palindrome("rutor")
+is_Palindrome("rotor")
